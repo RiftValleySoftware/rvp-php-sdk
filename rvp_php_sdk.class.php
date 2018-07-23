d1aa871c495b9fc778a60e72f7efb441b2b386d6
Chris M <webmaster@magnaws.com>
2018-07-23 07:21:39 -0400

    Youknowhat? No need for BASALT (except I may bring it back for testing).

---
+++ rvp_php_sdk.class.php
@@ -0,0 +1,30 @@
+<?php
+/***************************************************************************************************************************/
+/**
+    BASALT Extension Layer
+    
+    © Copyright 2018, Little Green Viper Software Development LLC.
+    
+    This code is proprietary and confidential code, 
+    It is NOT to be reused or combined into any application,
+    unless done so, specifically under written license from Little Green Viper Software Development LLC.
+
+    Little Green Viper Software Development: https://littlegreenviper.com
+*/
+defined( 'LGV_RVP_PHP_SDK_CATCHER' ) or die ( 'Cannot Execute Directly' );	// Makes sure that this file is in the correct context.
+
+define('__SDK_VERSION__', '1.0.0.0000');
+
+/****************************************************************************************************************************/
+/**
+ */
+class RVP_PHP_SDK {
+
+    /************************************************************************************************************************/    
+    /*#################################################### INTERNAL METHODS ################################################*/
+    /************************************************************************************************************************/
+    
+    /***********************/
+    /**
+     */
+};
