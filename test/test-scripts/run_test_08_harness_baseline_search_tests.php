c5c5847d43b729825cc6723dd791404d73877ff8
Chris M <webmaster@magnaws.com>
2018-07-30 07:26:51 -0400

    Added the skeleton of a new search test.

---
+++ test/test-scripts/run_test_08_harness_baseline_search_tests.php
@@ -0,0 +1,39 @@
+<?php
+/***************************************************************************************************************************/
+/**
+    BAOBAB PHP SDK
+    
+    © Copyright 2018, Little Green Viper Software Development LLC.
+    
+    This code is proprietary and confidential code, 
+    It is NOT to be reused or combined into any application,
+    unless done so, specifically under written license from Little Green Viper Software Development LLC.
+
+    Little Green Viper Software Development: https://littlegreenviper.com
+*/
+
+function run_test_08_harness_baseline_search_tests($test_harness_instance) {
+    $all_pass = true;
+    $test_count = $test_harness_instance->test_count + 1;
+    
+    if (isset($test_harness_instance->sdk_instance)) {
+        if ($test_harness_instance->sdk_instance->valid()) {
+        } else {
+            $all_pass = false;
+            $test_harness_instance->write_log_entry('VALIDITY CHECK', $test_count++, false);
+            echo('<h4 style="color:red">SERVER NOT VALID!</h4>');
+        }
+    } else {
+        $all_pass = false;
+        $test_harness_instance->write_log_entry('INSTANTIATION CHECK', $test_count++, false);
+        echo('<h4 style="color:red">NO SDK INSTANCE!</h4>');
+    }
+    
+    if ($all_pass) {
+        echo('<h4 style="color:green">TEST SUCCESSFUL!</h4>');
+    }
+    
+    $test_harness_instance->test_count = $test_count;
+    return $all_pass;     
+}
+?>
