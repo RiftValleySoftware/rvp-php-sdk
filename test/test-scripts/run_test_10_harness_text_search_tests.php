666972c0ce1de2d27bb4ffec3f153e21b427782c
Chris M <webmaster@magnaws.com>
2018-08-01 10:22:06 -0400

    Got up to tag 2.

--- test/test-scripts/run_test_10_harness_text_search_tests.php
+++ test/test-scripts/run_test_10_harness_text_search_tests.php
@@ -19,6 +19,14 @@ define('__SEARCH_SPEC_5__', ['tag0' => 'Grace United Methodist Church']);
 define('__RESULTS_5__', [['id' => 1592, 'type' => 'RVP_PHP_SDK_Place', 'name' => 'Hope Not Dope'],['id' => 1721, 'type' => 'RVP_PHP_SDK_Place', 'name' => 'Recovery in the AM']]);
 define('__SEARCH_SPEC_6__', ['tag0' => 'Grace % Church']);
 define('__RESULTS_6__', [['id' => 17, 'type' => 'RVP_PHP_SDK_Place', 'name' => 'You Get What You Need'], ['id' => 158, 'type' => 'RVP_PHP_SDK_Place', 'name' => 'Just the Desire'], ['id' => 274, 'type' => 'RVP_PHP_SDK_Place', 'name' => 'Lighthouse'], ['id' => 280, 'type' => 'RVP_PHP_SDK_Place', 'name' => 'Circle of Hope'], ['id' => 400, 'type' => 'RVP_PHP_SDK_Place', 'name' => 'Work Em Or Die'], ['id' => 949, 'type' => 'RVP_PHP_SDK_Place', 'name' => 'Last Connection'], ['id' => 1050, 'type' => 'RVP_PHP_SDK_Place', 'name' => 'Spiritual Principles'], ['id' => 1278, 'type' => 'RVP_PHP_SDK_Place', 'name' => 'NA @ Mill Creek'], ['id' => 1330, 'type' => 'RVP_PHP_SDK_Place', 'name' => 'Steps to Recovery'], ['id' => 1476, 'type' => 'RVP_PHP_SDK_Place', 'name' => 'New Gift Called Life'], ['id' => 1592, 'type' => 'RVP_PHP_SDK_Place', 'name' => 'Hope Not Dope'], ['id' => 1621, 'type' => 'RVP_PHP_SDK_Place', 'name' => 'Recovery in the AM'], ['id' => 1626, 'type' => 'RVP_PHP_SDK_Place', 'name' => 'Recovery in the AM'], ['id' => 1646, 'type' => 'RVP_PHP_SDK_Place', 'name' => 'Recovery in the AM'], ['id' => 1704, 'type' => 'RVP_PHP_SDK_Place', 'name' => 'Recovery in the AM'], ['id' => 1721, 'type' => 'RVP_PHP_SDK_Place', 'name' => 'Recovery in the AM']]);
+define('__SEARCH_SPEC_7__', ['tag1' => '265 East Main St.']);
+define('__RESULTS_7__', [['id' => 44, 'type' => 'RVP_PHP_SDK_Place', 'name' => 'Honest About Drugs']]);
+define('__SEARCH_SPEC_8__', ['tag1' => '265 East Main St%']);
+define('__RESULTS_8__', [['id' => 44, 'type' => 'RVP_PHP_SDK_Place', 'name' => 'Honest About Drugs'], ['id' => 164, 'type' => 'RVP_PHP_SDK_Place', 'name' => 'Who We Are']]);
+define('__SEARCH_SPEC_9__', ['tag2' => 'TAG-2-TEST-PEOPLE']);
+define('__RESULTS_9__', [['id' => 1725, 'type' => 'RVP_PHP_SDK_User', 'name' => 'MDAdmin'], ['id' => 1727, 'type' => 'RVP_PHP_SDK_User', 'name' => 'DCAdmin'], ['id' => 1729, 'type' => 'RVP_PHP_SDK_User', 'name' => 'DEAdmin']]);
+define('__SEARCH_SPEC_10__', ['tag2' => 'TAG-2-TEST-%']);
+define('__RESULTS_10__', [['id' => 1725, 'type' => 'RVP_PHP_SDK_User', 'name' => 'MDAdmin'], ['id' => 1727, 'type' => 'RVP_PHP_SDK_User', 'name' => 'DCAdmin'], ['id' => 1729, 'type' => 'RVP_PHP_SDK_User', 'name' => 'DEAdmin'], ['id' => 1732, 'type' => 'RVP_PHP_SDK_Thing', 'name' => 'Worth Enough'], ['id' => 1733, 'type' => 'RVP_PHP_SDK_Thing', 'name' => 'Another World'], ['id' => 1735, 'type' => 'RVP_PHP_SDK_Thing', 'name' => 'The Great Shadow'], ['id' => 1736, 'type' => 'RVP_PHP_SDK_Thing', 'name' => 'Yosemite'], ['id' => 1737, 'type' => 'RVP_PHP_SDK_Thing', 'name' => 'Tom And Jerry'], ['id' => 1739, 'type' => 'RVP_PHP_SDK_Thing', 'name' => 'Winnie The Pooh'], ['id' => 1740, 'type' => 'RVP_PHP_SDK_Thing', 'name' => 'Crickets'], ['id' => 1741, 'type' => 'RVP_PHP_SDK_Thing', 'name' => 'Singing Pete'], ['id' => 1742, 'type' => 'RVP_PHP_SDK_Thing', 'name' => 'Spinning Earth'], ['id' => 1743, 'type' => 'RVP_PHP_SDK_Thing', 'name' => 'The Three Musketeers In Dutch']]);
 
 function run_test_10_harness_text_search_tests($test_harness_instance) {
     $all_pass = true;
@@ -64,10 +72,10 @@ function run_test_10_harness_text_search_tests($test_harness_instance) {
             }
 
             if (RVP_PHP_SDK_Test_Harness::are_arrays_equal(__RESULTS_5__, $dump)) {
-                $test_harness_instance->write_log_entry('Simple Baseline General Wildcard Text Search', $test_count++, true);
+                $test_harness_instance->write_log_entry('Simple Baseline General Exact Text Search', $test_count++, true);
             } else {
                 $all_pass = false;
-                $test_harness_instance->write_log_entry('Simple Baseline General Wildcard Text Search', $test_count++, false);
+                $test_harness_instance->write_log_entry('Simple Baseline General Exact Text Search', $test_count++, false);
                 echo('<h4 style="color:red">RESPONSE DATA INVALID!</h4>');
             }
             
@@ -79,6 +87,67 @@ function run_test_10_harness_text_search_tests($test_harness_instance) {
             }
 
             if (RVP_PHP_SDK_Test_Harness::are_arrays_equal(__RESULTS_6__, $dump)) {
+                $test_harness_instance->write_log_entry('Simple Baseline General Wildcard Text Search', $test_count++, true);
+            } else {
+                $all_pass = false;
+                $test_harness_instance->write_log_entry('Simple Baseline General Wildcard Text Search', $test_count++, false);
+                echo('<h4 style="color:red">RESPONSE DATA INVALID!</h4>');
+            }
+            
+            echo('<h4>Do A Search for Records with Tag 1 of "265 East Main St."</h4>');
+            $results = $test_harness_instance->sdk_instance->general_text_search(__SEARCH_SPEC_7__);
+            $dump = [];
+            foreach ($results as $node) {
+                $dump[] = ['id' => $node->id(), 'type' => get_class($node), 'name' => $node->name()];
+            }
+
+            if (RVP_PHP_SDK_Test_Harness::are_arrays_equal(__RESULTS_7__, $dump)) {
+                $test_harness_instance->write_log_entry('Simple Baseline General Exact Text Search', $test_count++, true);
+            } else {
+                $all_pass = false;
+                $test_harness_instance->write_log_entry('Simple Baseline General Exact Text Search', $test_count++, false);
+                echo('<h4 style="color:red">RESPONSE DATA INVALID!</h4>');
+            }
+            
+            echo('<h4>Do A Search for Records with Tag 1 set to a Wildcard of "265 East Main St%"</h4>');
+            $results = $test_harness_instance->sdk_instance->general_text_search(__SEARCH_SPEC_8__);
+            $dump = [];
+            foreach ($results as $node) {
+                $dump[] = ['id' => $node->id(), 'type' => get_class($node), 'name' => $node->name()];
+            }
+
+            if (RVP_PHP_SDK_Test_Harness::are_arrays_equal(__RESULTS_8__, $dump)) {
+                $test_harness_instance->write_log_entry('Simple Baseline General Exact Text Search', $test_count++, true);
+            } else {
+                $all_pass = false;
+                $test_harness_instance->write_log_entry('Simple Baseline General Exact Text Search', $test_count++, false);
+                echo('<h4 style="color:red">RESPONSE DATA INVALID!</h4>');
+            }
+            
+            echo('<h4>Do A Search for Records with Tag 2 of "TAG-2-TEST-PEOPLE"</h4>');
+            $results = $test_harness_instance->sdk_instance->general_text_search(__SEARCH_SPEC_9__);
+            $dump = [];
+            foreach ($results as $node) {
+                $dump[] = ['id' => $node->id(), 'type' => get_class($node), 'name' => $node->name()];
+            }
+
+            if (RVP_PHP_SDK_Test_Harness::are_arrays_equal(__RESULTS_9__, $dump)) {
+                $test_harness_instance->write_log_entry('Simple Baseline General Exact Text Search', $test_count++, true);
+            } else {
+                $all_pass = false;
+                $test_harness_instance->write_log_entry('Simple Baseline General Exact Text Search', $test_count++, false);
+                echo('<h4 style="color:red">RESPONSE DATA INVALID!</h4>');
+            }
+            
+            echo('<h4>Do A Search for Records with Tag 2 of "TAG-2-TEST-%"</h4>');
+            $results = $test_harness_instance->sdk_instance->general_text_search(__SEARCH_SPEC_10__);
+            $dump = [];
+            foreach ($results as $node) {
+                $dump[] = ['id' => $node->id(), 'type' => get_class($node), 'name' => $node->name()];
+            }
+
+// echo('<pre>'.print_r($dump, true).'</pre>');
+            if (RVP_PHP_SDK_Test_Harness::are_arrays_equal(__RESULTS_10__, $dump)) {
                 $test_harness_instance->write_log_entry('Simple Baseline General Wildcard Text Search', $test_count++, true);
             } else {
                 $all_pass = false;
