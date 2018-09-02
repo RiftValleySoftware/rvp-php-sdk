bd5c4356bf29e1231d1ecf09df0537ba65c6f528
Chris M <webmaster@magnaws.com>
2018-09-02 19:12:43 -0400

    Added an image display to the last test.

--- test/test-scripts/run_test_35_post_things_tests.php
+++ test/test-scripts/run_test_35_post_things_tests.php
@@ -45,6 +45,8 @@ function run_test_35_post_things_tests($test_harness_instance) {
         
         if (isset($new_thing)) {
             $test_harness_instance->write_log_entry('UNIQUE KEY CHECK', $test_count++, true);
+            $payload = $new_thing->payload();
+            run_test_35_harness_get_payload_tests_display_image($payload['data'], $payload['type']);
         } else {
             $all_pass = false;
             $test_harness_instance->write_log_entry('UNIQUE KEY CHECK', $test_count++, false);
@@ -63,5 +65,33 @@ function run_test_35_post_things_tests($test_harness_instance) {
     $test_harness_instance->test_count = $test_count;
     
     return $all_pass;     
+}
+
+function run_test_35_harness_get_payload_tests_display_image($in_image_data, $in_image_type) {
+    $id = uniqid('test-result-');
+    $file_suffix = 'fa';
+    switch ($in_image_type) {
+        case 'image/jpeg':
+            $file_suffix = 'jpg';
+            break;
+        case 'image/png':
+            $file_suffix = 'png';
+            break;
+        case 'image/gif':
+            $file_suffix = 'gif';
+            break;
+    }
+    
+    echo('<div id="'.$id.'" class="inner_closed">');
+        echo('<h3 class="inner_header"><a href="javascript:toggle_inner_state(\''.$id.'\')">Display Image</a></h3>');
+        echo('<div class="inner_container">');
+            echo('<div class="container">');
+                $fname = 'run_test_35_'.$id.'.'.$file_suffix;
+                file_put_contents(dirname(dirname(__FILE__)).'/tmp/'.$fname, $in_image_data);
+                
+                echo('<div class="image_display_div"><img src="tmp/'.$fname.'" title="Image Payload" alt="Image Payload" style="max-width:100%" /></div>');
+            echo('</div>');
+        echo('</div>');
+    echo('</div>');
 }
 ?>
